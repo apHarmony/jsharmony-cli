0c87fa7f2f43b179191563825daf38990674e4d3
Andrew Palczewski <apj@apharmony.com>
2018-10-27 14:34:07 -0500

    Add SQL extension to auto-restart

--- cli.create-factory.js
+++ cli.create-factory.js
@@ -159,7 +159,7 @@ exports.Run = function(params, onSuccess){
 
   //Create nstart
   .then(function(){ return new Promise(function(resolve, reject){
-    var rslt = 'supervisor -i test,public,data -w "./models,./views,./app.config.js,./app.js" -e "node,js,json,css" node "./app.js"';
+    var rslt = 'supervisor -i test,public,data -w "./models,./views,./app.config.js,./app.js" -e "node,js,json,css,sql" node "./app.js"';
     fs.writeFileSync(jshconfig.path+'/'+global._NSTART_CMD, rslt);
     if(!global._IS_WINDOWS) fs.chmodSync(jshconfig.path+'/'+global._NSTART_CMD, '755');
     resolve();
